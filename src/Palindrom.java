8f3a0c7efac4b923aec78ff724cffbd016a29caa
Yevgeniy <43024182+YevgeniyBoykov@users.noreply.github.com>
2018-09-21 17:44:12 +0300

    Update Palindrom.java

--- src/Palindrom.java
+++ src/Palindrom.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 import static java.lang.Math.floor;
 
 public class Palindrom
@@ -9,25 +8,34 @@ public class Palindrom
         Scanner in = new Scanner(System.in);
         System.out.println("Enter string: ");
         String aspal = in.nextLine();
-        char[] aspala = aspal.toCharArray();
+
+        int res = Palindrom(aspal);
+    }
+
+    private static int Palindrom(String stroka)
+    {
         short result = 0;
         int symb = 0;
 
-       do
-       {
-           if(aspala[symb] == aspala[aspal.length()-symb-1])
-           {
-               result++;
-               if (result == floor(aspal.length()/2))
-                System.out.println("It's a pollyndrom!!!");
-           }
-           else
-           {
-               System.out.println("It's NOT!");
-               break;
-           }
-           symb++;
+        do
+        {
+            if(stroka.charAt(symb) == stroka.charAt(stroka.length()-symb-1))
+            {
+                result++;
+                if (result == floor(stroka.length()/2))
+                {
+                    System.out.println("It's a pollyndrom!!!");
+                    break;
+                }
+            }
+            else
+            {
+                System.out.println("It's NOT!");
+                break;
+            }
+            symb++;
 
-        } while (symb < aspal.length()/2);
+        } while (symb < stroka.length()/2);
+        return 0;
     }
  }
