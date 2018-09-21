f4596860c413b9a3afe78b78df36032fc9732979
Yevgeniy <43024182+YevgeniyBoykov@users.noreply.github.com>
2018-09-21 12:40:51 +0300

    Update Palindrom.java

--- src/Palindrom.java
+++ src/Palindrom.java
@@ -1,40 +1,33 @@
 import java.util.Scanner;
 
-import static java.lang.Math.rint;
+import static java.lang.Math.floor;
 
 public class Palindrom
 {
-    public static void main(String[] args)
+     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter string: ");
         String aspal = in.nextLine();
         char[] aspala = aspal.toCharArray();
         short result = 0;
+        int symb = 0;
 
-        for(int symb = 0; symb < aspal.length()/2; symb++)
-        {
+       do
+       {
+           if(aspala[symb] == aspala[aspal.length()-symb-1])
+           {
+               result++;
+               if (result == floor(aspal.length()/2))
+                System.out.println("It's a pollyndrom!!!");
+           }
+           else
+           {
+               System.out.println("It's NOT!");
+               break;
+           }
+           symb++;
 
-                if(aspalinom (aspala[symb], aspala[aspal.length()-symb-1])==1)
-                {
-                    result++;
-                }
-
-        }
-
-        if (result == rint(aspal.length()/2))
-            System.out.println("It's a pollyndrom!!!");
-        else
-            System.out.println("It's NOT!");
-
+        } while (symb < aspal.length()/2);
     }
-
-    private static int aspalinom(char first, char second)
-    {
-        if(first == second)
-            return 1;
-        else
-            return 0;
-    }
-
-}
+ }
